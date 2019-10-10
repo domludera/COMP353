bf99c88c0356a30fb5d0a8cd746ea12ec2a58f71
root <root@localhost.localdomain>
2019-10-10 11:42:16 -0400

    parser for student

--- Assg3/gen/datagen.py
+++ Assg3/gen/datagen.py
@@ -1,5 +1,31 @@
-import os.path
+import random, sys
 
-file = open('lists/names.txt','r')
+nameFile = open('lists/names.txt','r')
 
-print(file.read())
+
+#Student(sid, sname, email)
+# format -> sid|sname|email
+studentTable = open('tables/student.txt','w')
+b = True
+i = 0
+while b:
+    try:    
+        name = str(nameFile.readline()).strip('\n')
+        if(name==""):
+            break
+        email = name+"@gmail.com"
+        studentTable.write("{}|{}|{}\n".format(i, name, email))
+        i += 1
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+     #b=False
+
+#Enrollment(sid, cid, grade)
+enrollmentTable = open('tables/enrollment.txt','w')
+
+#Course(cid, cname, year, term, section, credits)
+courseTable = open('tables/course.txt','w')
+
+#Teach(cid, ProfName)
+teachTable = open('tables/teach.txt','w')
