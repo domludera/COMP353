39e54cd3ce2356afcc8494ffd5ee1befa6546842
lilla021 <priscilla.cournoyer@hotmail.com>
2019-12-07 18:42:38 -0500

    Update usersController.php
    fixed silly exhaustion errors

--- Controllers/usersController.php
+++ Controllers/usersController.php
@@ -11,37 +11,23 @@ class usersController extends Controller {
         $this->render("index");
     }
 
-    function update() {
-        $this->authed();
-        require(ROOT . 'Models/User.php');
-        $user = new User();
-        $id = $_SESSION["user"];
-        //var_dump($_FILES);exit;
-        if (isset($_POST["email"])) {
-            if ($user->edit($id, $_POST["email"], $_POST["profession"], $_POST["dob"], $_POST["password"], $_POST["name"], $_POST["region"], $_FILES["fileToUpload"])) {
-                header("Location: " . WEBROOT . "users/self");
-            }
-        }
-
-        $results["user"] = User::resultToArray($user->find($_SESSION["user"]))[0];
-        $this->set($results);
-        $this->render("self");
-    }
-
     function edit() {
         $this->authed();
         // METHOD: POST
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if (!class_exists('Mail')) {
-                require(ROOT . 'Models/User.php');
+            require_once(ROOT . 'Models/User.php');
+            $user = new User();
+            $id = $_SESSION["user"];
+            //var_dump($_FILES);exit;
+            if (isset($_POST["email"])) {
+                if ($user->edit($id, $_POST["email"], $_POST["profession"], $_POST["dob"], $_POST["name"], $_POST["region"], $_FILES["fileToUpload"])) {
+                    $this->render("self");
+                }
             }
-            //require(ROOT . 'Models/Mail.php');
-            $user = new Mail();
-            $results = $user->create(
-                    $_SESSION["user"], $_POST["to"], $_POST["subject"], $_POST["content"]
-            );
-            // echo json_encode(Mail::resultToArray($results)[0]);
-            $this->redirect("/users");
+
+            $results["user"] = User::resultToArray($user->find($_SESSION["user"]))[0];
+            $this->set($results);
+            $this->render("self");
         }
 
         // METHOD: GET
